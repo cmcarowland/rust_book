74e208384f381e924d55c436cb17bf4ee03531fc
George Rowland <cmcarowland@live.com>
2022-06-26 21:31:06 -0400

    Functionized circle functionality
    refactor: Diameter, circumference and area to functions.
    feat: Added loop and checking if the user wants to continue
    feat: Created constant for PI.

--- circle_calculator/src/main.rs
+++ circle_calculator/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::io::Write;
 
+const PI : f32 = 3.14159265359;
+
 struct Rectangle {
     width: f32,
     height: f32,
@@ -23,12 +25,8 @@ impl Shape for Rectangle {
     }
 }
 
-fn main() {
+fn get_user_radius() -> String {
     let mut radius = String::new();
-    let pi = 3.14;
-    let diameter;
-    let circumference;
-    let area;
 
     print!("Enter Radius >> ");
     io::stdout().flush().unwrap();
@@ -36,24 +34,69 @@ fn main() {
     io::stdin().read_line(&mut radius)
         .expect("Unable to get string");
 
+    radius
+}
+
+fn check_continue() -> bool {
+    let mut radius = String::new();
+
+    print!("Continue? (y/n) >> ");
+    io::stdout().flush().unwrap();
+
+    io::stdin().read_line(&mut radius)
+        .expect("Unable to get string");
+
+    radius.chars().nth(0).unwrap() == 'y'
+}
+
+fn convert_radius_to_float(radius : &String) -> f32 {
     let radius: f32 = match radius.trim().parse(){
         Ok(num) => num,
         Err(_) => {
             println!("Not a number");
-            return;
+            -1.0
         },
     };
 
-    diameter = radius * 2.0;
-    circumference = diameter * pi;
-    area = pi * f32::powf(radius, 2.0);
+    radius
+}
 
-    println!("Radius : {}", radius);
-    println!("Diameter : {}", diameter);
-    println!("Circumference : {}", circumference);
-    println!("Area : {}", area);
+fn calculate_diameter(radius: f32) -> f32 {
+    radius * 2.0
+}
 
-    let mut r = Rectangle::new(3.0, 4.0);
-    r.calculate();
-    println!("Rect Area : {}", r.area); 
+fn calculate_circumference(diameter: f32) -> f32 {
+    diameter * PI
+}
+
+fn calculate_area(radius: f32) -> f32 {
+    PI * f32::powf(radius, 2.0)
+}
+
+fn main() {
+    let mut diameter;
+    let mut circumference;
+    let mut area;
+    let mut is_running : bool = true;
+
+    while is_running {
+        let radius = get_user_radius();
+
+        let radius = convert_radius_to_float(&radius);
+
+        diameter = calculate_diameter(radius);
+        circumference = calculate_circumference(diameter);
+        area = calculate_area(radius);
+
+        println!("Radius : {}", radius);
+        println!("Diameter : {}", diameter);
+        println!("Circumference : {}", circumference);
+        println!("Area : {}", area);
+
+        let mut r = Rectangle::new(3.0, 4.0);
+        r.calculate();
+        println!("Rect Area : {}", r.area); 
+
+        is_running = check_continue();
+    }
 }
