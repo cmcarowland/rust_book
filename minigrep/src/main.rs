0b3f6c253c6ba1cc7926d55239bd4768a7ec0ec1
George Rowland <cmcarowland@live.com>
2022-07-21 20:03:57 -0400

    Refactored main
    refactor: Config to have a new function.
    refactor: Order of print statements.
    feat: New Config struct to hold configuration data.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,18 +4,26 @@ use std::fs;
 fn main() {
     let args : Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file.");
 
-    println!("Searching for {}", query);
-    println!("In File {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In File {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file.");
     println!("With Text :\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query : String,
+    filename : String,
+}
 
-    (query, filename)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config{ query, filename }
+    }
 }
