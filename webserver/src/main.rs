75170b1e7883978286d4b477bef19235d2f728af
George Rowland <cmcarowland@live.com>
2022-08-07 08:48:39 -0400

    Created basic webserver
    feat: Accept incoming connections and print to terminal.

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let tcplistener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in tcplistener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection Received!!!");
+    }
 }
