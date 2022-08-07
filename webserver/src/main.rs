bc33557222584c1d5ef6109f8a619adaddb8de25
George Rowland <cmcarowland@live.com>
2022-08-07 08:53:32 -0400

    Added ability to read HTTP requests
    feat: Added tcp stream reading into project.
    feat: Print the http request to console.
    refactor: Updated main to use the handle_connection function.

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
 
 fn main() {
     let tcplistener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +8,14 @@ fn main() {
     for stream in tcplistener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Received!!!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request received : {}", String::from_utf8_lossy(&buffer[..]));
 }
