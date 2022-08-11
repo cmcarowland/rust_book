f16839dd69d47520e7e4513418cb200cbccfb8f9
George Rowland <cmcarowland@live.com>
2022-08-11 18:48:34 -0400

    Completed project 8-1 from C++ book
    feat: Added grade book functionality.
    feat: Stores student grades in a vector and averages them.
    feat: Allows users to enter scores until they are ready to stop.

--- gradebook/src/main.rs
+++ gradebook/src/main.rs
@@ -1,3 +1,47 @@
+//Make a gradebook application that can 
+//[ ] Take user input for a series of grades
+//[ ] Return the average if a negative number is entered.
+
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut scores : Vec<i32> = Vec::new();
+    let mut grade: String = String::new();
+
+    println!("Enter Grades (0 or less to exit)");
+    io::stdout().flush().unwrap();
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+
+        grade = String::new();
+        io::stdin().read_line(&mut grade)
+            .expect("Failed To Read Line.");
+
+        let grade: i32 = match grade.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid Number Entered!");
+                return;
+            }
+        };
+
+        if grade > 0 {
+            add_score(grade, &mut scores);
+        } else {
+            break;
+        }
+    }
+
+    println!("Scores Entered {:?}", scores);
+    println!("Score {:.2}", average_scores(&scores));
+}
+
+fn add_score(score: i32, scores: &mut Vec<i32>) {
+    scores.push(score);
+}
+
+fn average_scores(scores : &Vec<i32>) -> f32 {
+    (scores.iter().sum::<i32>() as f32) / (scores.len() as f32)
 }
